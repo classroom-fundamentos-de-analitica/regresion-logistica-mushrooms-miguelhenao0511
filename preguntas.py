0635eb89a90bd60988b1bc42d5c24767e3200ff8
Miguel Henao <mhenaoh@unal.edu.co>
2022-11-28 07:57:32 -0500

    Solution

--- preguntas.py
+++ preguntas.py
@@ -60,113 +60,68 @@ import pandas as pd
 
 
 def pregunta_01():
-    """
-    En esta función se realiza la carga de datos.
-    """
-    # Lea el archivo `mushrooms.csv` y asignelo al DataFrame `df`
-    df = ____
-
-    # Remueva la columna `veil-type` del DataFrame `df`.
-    # Esta columna tiene un valor constante y no sirve para la detección de hongos.
-    ____.____(____)
-
-    # Asigne la columna `type` a la variable `y`.
-    ____ = ____
-
-    # Asigne una copia del dataframe `df` a la variable `X`.
-    ____ = ____.____(____)
-
-    # Remueva la columna `type` del DataFrame `X`.
-    ____.____(____)
-
-    # Retorne `X` y `y`
+    df = pd.read_csv('mushrooms.csv',sep=",")
+    
+    df.drop('veil_type',axis=1,inplace=True)
+    
+    y = df['type']
+    
+    X = df.copy(deep=True)
+    
+    X.drop('type',axis=1,inplace=True)
+    
     return X, y
 
 
 def pregunta_02():
-    """
-    Preparación del dataset.
-    """
-
-    # Importe train_test_split
-    from ____ import ____
-
-    # Cargue los datos de ejemplo y asigne los resultados a `X` y `y`.
+    from sklearn.model_selection import train_test_split
+    
     X, y = pregunta_01()
-
-    # Divida los datos de entrenamiento y prueba. La semilla del generador de números
-    # aleatorios es 123. Use 50 patrones para la muestra de prueba.
-    (X_train, X_test, y_train, y_test,) = ____(
-        ____,
-        ____,
-        test_size=____,
-        random_state=____,
+    
+    (X_train, X_test, y_train, y_test,) = train_test_split(
+        X,
+        y,
+        test_size=50,
+        random_state=123,
     )
-
-    # Retorne `X_train`, `X_test`, `y_train` y `y_test`
+    
     return X_train, X_test, y_train, y_test
 
 
 def pregunta_03():
-    """
-    Especificación y entrenamiento del modelo. En sklearn, el modelo de regresión
-    logística (a diferencia del modelo implementado normalmente en estadística) tiene
-    un hiperparámetro de regularición llamado `Cs`. Consulte la documentación.
-
-    Para encontrar el valor óptimo de Cs se puede usar LogisticRegressionCV.
-
-    Ya que las variables explicativas son literales, resulta más conveniente usar un
-    pipeline.
-    """
-
-    # Importe LogisticRegressionCV
-    # Importe OneHotEncoder
-    # Importe Pipeline
-    from ____ import ____
-
-    # Cargue las variables.
-    X_train, _, y_train, _ = pregunta_02()
-
-    # Cree un pipeline que contenga un estimador OneHotEncoder y un estimador
-    # LogisticRegression con una regularización Cs=10
+    from sklearn.linear_model import LogisticRegressionCV
+    from sklearn.preprocessing import OneHotEncoder
+    from sklearn.pipeline import Pipeline
+    
+    X_train, X_test, y_train, y_test = pregunta_02()
+    
     pipeline = Pipeline(
         steps=[
-            ("____", ____()),
-            ("____", ____(____)),
+            ("onehot", OneHotEncoder()),
+            ("logisticRegression", LogisticRegressionCV(Cs=10)),
         ],
     )
-
-    # Entrene el pipeline con los datos de entrenamiento.
-    ____.____(____, ____)
-
-    # Retorne el pipeline entrenado
+    
+    pipeline.fit(X_train, y_train)
+    
     return pipeline
 
 
 def pregunta_04():
-    """
-    Evalue el modelo obtenido.
-    """
-
-    # Importe confusion_matrix
-    from ____ import ____
-
-    # Obtenga el pipeline de la pregunta 3.
+    from sklearn.metrics import confusion_matrix
+    
     pipeline = pregunta_03()
-
-    # Cargue las variables.
+    
     X_train, X_test, y_train, y_test = pregunta_02()
-
-    # Evalúe el pipeline con los datos de entrenamiento usando la matriz de confusion.
-    cfm_train = ____(
-        y_true=____,
-        y_pred=____.____(____),
+    
+    cfm_train = confusion_matrix(
+        y_true=y_train,
+        y_pred=pipeline.predict(X_train),
     )
 
-    cfm_test = ____(
-        y_true=____,
-        y_pred=____.____(____),
+    cfm_test = confusion_matrix(
+        y_true=y_test,
+        y_pred=pipeline.predict(X_test),
     )
-
-    # Retorne la matriz de confusion de entrenamiento y prueba
+    
     return cfm_train, cfm_test
